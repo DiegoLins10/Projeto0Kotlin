073f08f46439cb6977518f17060869af35da1f48
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-03-24 11:42:57 -0300

    first project kotlin

---
+++ app/src/main/java/com/example/primeiroprojeto/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.primeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //vl texto = TextView(this)
+        //texto.text = "Hello Fatec"
+        //setContentView(texto)
+        }
+     }
