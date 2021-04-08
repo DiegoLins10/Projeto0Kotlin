8d04998f21c0e417e0c2a89fa8ce606506a5e9c6
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-04-07 21:13:22 -0300

    3 alterações atividade

--- app/src/main/java/com/example/primeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/example/primeiroprojeto/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.primeiroprojeto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.*
+import org.w3c.dom.Text
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //vl texto = TextView(this)
-        //texto.text = "Hello Fatec"
-        //setContentView(texto)
+
+        val txtA = findViewById<TextView>(R.id.txt_A)
+        val txtB = findViewById<TextView>(R.id.txt_B)
+        val btn_Calcular = findViewById<Button>(R.id.btn_Calcular)
+        val txt_Resultado = findViewById<TextView>(R.id.txt_Resultado)
+
+
+        //btn_Calcular_onclick
+        btn_Calcular.setOnClickListener{
+            val A = txtA.text.toString().toInt()
+            val B = txtB.text.toString().toInt()
+            var resultado = A + B
+
+            txt_Resultado.text = "Resultado: $resultado"
+
         }
-     }
+    }
+}
